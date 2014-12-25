e373326d172608c17154214d0b8cc709f270bd5d
Joe-noh <goflb.jh@gmail.com>
2014-12-26 07:35:21 +0900

    create skel of compile and init tasks

---
+++ test/tasks_test.exs
@@ -0,0 +1,11 @@
+defmodule TasksTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "mix help shows diethyl tasks" do
+    tasks = capture_io(fn -> Mix.shell.cmd "mix help" end)
+
+    assert tasks =~ ~r{diethyl\.compile}
+    assert tasks =~ ~r{diethyl\.init}
+  end
+end
